0613cd5388c99c7314ff96dbdca8bf724c7b191a
Hamza <hamzaaddami11@gmail.com>
2026-01-03 16:40:25 +0100

    Merging hamza

--- services/api.js
+++ services/api.js
@@ -3,7 +3,13 @@ import axios from 'axios';
 import * as SecureStore from 'expo-secure-store';
 import { Platform } from 'react-native';
 
+<<<<<<< HEAD
 const API_URL = 'http://192.168.137.73:5182/api';
+=======
+const Ip = "192.168.137.64";
+
+const API_URL = `http://${Ip}:5182/api`;
+>>>>>>> fcbcfeb (Fixing Ip)
 
 const api = axios.create({
   baseURL: API_URL,
@@ -19,7 +25,12 @@ api.interceptors.request.use(async (config) => {
 });
 
 export const HubUrl = Platform.OS === 'android' 
+<<<<<<< HEAD
   ? API_URL 
   : API_URL;
+=======
+  ? `http://${Ip}:5182/hubs/chat` 
+  : `http://${Ip}:5182/hubs/chat`;
+>>>>>>> fcbcfeb (Fixing Ip)
 
 export default api;
